e85e70da1a27b095c24a58b575416471bf1b600d
raysalarify-sketch <ray.salarify@gmail.com>
2026-02-19 18:45:50 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // 여기서 App.js를 불러옵니다.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
